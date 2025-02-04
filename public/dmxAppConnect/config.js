7604299d393a309f195788950a11cc29405589be
Pro Bro <kopitik667@busantei.com>
2025-02-04 04:53:28 +0300

    settup 1 road

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,29 @@
+dmx.config({
+  "setting": {
+    "data_detail_vid_sporta": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "user_uuid"
+        },
+        {
+          "type": "text",
+          "name": "vid_sporta"
+        },
+        {
+          "type": "text",
+          "name": "vid_sporta_sis"
+        },
+        {
+          "type": "number",
+          "name": "sorting"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
